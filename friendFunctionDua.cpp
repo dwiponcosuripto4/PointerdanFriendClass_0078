144bead9c023fd66414f57396ded0225c54caa44
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-06 16:10:21 +0700

    friendfunctiondua

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -1,3 +1,33 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class pelajar;
+class manusia
+{
+public:
+    void showNilaiPelajar(pelajar &a);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &a);
+};
+
+void manusia::showNilaiPelajar(pelajar &a)
+{
+    cout << a.nilai;
+}
+
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
