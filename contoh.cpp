1fc85757be7077962aa55911a092f7abcf623f67
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-06 15:11:26 +0700

    create func input persegi

--- contoh.cpp
+++ contoh.cpp
@@ -6,4 +6,13 @@ class PersegiPanjang
 
 public:
     int panjang, lebar;
+
+public:
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan lebar : ";
+        cin >> lebar;
+    }
 };
