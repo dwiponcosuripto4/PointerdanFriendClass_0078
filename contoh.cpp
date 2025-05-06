1f6778db1e170ed608617517f4e9e27e50c3e37c
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-06 15:16:09 +0700

    membuat objek

--- contoh.cpp
+++ contoh.cpp
@@ -24,4 +24,23 @@ public:
 
 class Lingkaran
 {
+public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin >> jarijari;
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
+};
+
+int main()
+{
+    PersegiPanjang objekPP;
+    Lingkaran el;
 };
