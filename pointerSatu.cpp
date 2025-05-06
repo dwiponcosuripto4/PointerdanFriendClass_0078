14531e5b2191a319f1b1a8ec9c6bc3a428e815ab
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-06 19:46:55 +0700

    fix pointer satu

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -13,14 +13,14 @@ public:
 
 int main()
 {
-    mahasiswa mhs(1);
+    mahasiswa mhs{1};
     mhs.showNim();
 
     mahasiswa &refMhs = mhs;
     refMhs.nim = 2;
     mhs.showNim();
 
-    mahasiswa pMhs = &mhs;
+    mahasiswa *pMhs = &mhs;
     pMhs->nim = 3;
     pMhs->showNim();
     return 0;
